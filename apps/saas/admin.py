3dd191745b1e786c45c34a484e034472d3b48865
Mohammad Sakib Howlader <nawabsakib5@gmail.com>
2026-09-11 15:52:47 +0600

    Add: saas and accounts admin registration

--- apps/saas/admin.py
+++ apps/saas/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Tenant, Domain
 
-# Register your models here.
+
+@admin.register(Tenant)
+class TenantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'schema_name', 'contact_email', 'contact_phone', 'is_active', 'created_on')
+    list_filter = ('is_active',)
+    search_fields = ('name', 'contact_email')
+
+
+@admin.register(Domain)
+class DomainAdmin(admin.ModelAdmin):
+    list_display = ('domain', 'tenant', 'is_primary')
+    list_filter = ('is_primary',)
+    search_fields = ('domain',)
