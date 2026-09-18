abbeb1f983de6a4d355f9a1ef384e995f6450520
Mohammad Sakib Howlader <nawabsakib5@gmail.com>
2026-09-19 01:23:34 +0600

    staff_attendance_list.html

--- apps/examination/urls.py
+++ apps/examination/urls.py
@@ -1,3 +1,18 @@
 from django.urls import path
+from . import views
+
 app_name = 'examination'
-urlpatterns = []
+
+urlpatterns = [
+    # Exam
+    path('', views.exam_list, name='exam_list'),
+    path('add/', views.exam_add, name='exam_add'),
+
+    # Result
+    path('result/', views.result_list, name='result_list'),
+    path('result/add/<int:exam_pk>/', views.result_add, name='result_add'),
+
+    # Admit Card
+    path('admit-card/', views.admit_card_list, name='admit_card_list'),
+    path('admit-card/generate/<int:exam_pk>/', views.admit_card_generate, name='admit_card_generate'),
+]
