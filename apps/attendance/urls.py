1fb693735caf7505b92ebfaff91aa053c6a4cd38
Mohammad Sakib Howlader <nawabsakib5@gmail.com>
2026-09-19 01:19:48 +0600

    student_attendance_mark.html

--- apps/attendance/urls.py
+++ apps/attendance/urls.py
@@ -1,3 +1,15 @@
 from django.urls import path
+from . import views
+
 app_name = 'attendance'
-urlpatterns = []
+
+urlpatterns = [
+    # Student Attendance
+    path('student/', views.student_attendance_list, name='student_attendance_list'),
+    path('student/mark/', views.student_attendance_mark, name='student_attendance_mark'),
+
+    # Staff Attendance
+    path('staff/', views.staff_attendance_list, name='staff_attendance_list'),
+    path('staff/mark/', views.staff_attendance_mark, name='staff_attendance_mark'),
+    path('staff/import/', views.staff_attendance_import, name='staff_attendance_import'),
+]
