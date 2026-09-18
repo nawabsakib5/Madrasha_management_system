d1ca3ae722ec98a9bd4d313b53a95085bd568449
Mohammad Sakib Howlader <nawabsakib5@gmail.com>
2026-09-18 15:44:09 +0600

    fee_payment_form.html

--- apps/accounting/urls.py
+++ apps/accounting/urls.py
@@ -1,3 +1,22 @@
 from django.urls import path
+from . import views
+
 app_name = 'accounting'
-urlpatterns = []
+
+urlpatterns = [
+    # Fee Category
+    path('fee-category/', views.fee_category_list, name='fee_category_list'),
+    path('fee-category/add/', views.fee_category_add, name='fee_category_add'),
+
+    # Fee Invoice
+    path('fee-invoice/', views.fee_invoice_list, name='fee_invoice_list'),
+    path('fee-invoice/add/', views.fee_invoice_add, name='fee_invoice_add'),
+
+    # Fee Payment
+    path('fee-payment/', views.fee_payment_list, name='fee_payment_list'),
+    path('fee-payment/add/<int:invoice_pk>/', views.fee_payment_add, name='fee_payment_add'),
+
+    # Cash Book
+    path('cashbook/', views.cashbook_list, name='cashbook_list'),
+    path('cashbook/add/', views.cashbook_add, name='cashbook_add'),
+]
